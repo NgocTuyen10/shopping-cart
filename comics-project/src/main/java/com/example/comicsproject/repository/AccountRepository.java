d292a06276910730da516382c0ea9897c6f92b87
NgocTuyen10 <ngoisaosang26@gmail.com>
2019-09-24 09:50:22 +0700

    add customer account success

--- comics-project/src/main/java/com/example/comicsproject/repository/AccountRepository.java
+++ comics-project/src/main/java/com/example/comicsproject/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.example.comicsproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,7 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
 	@Query(value = "select * from account where username = :username and password = :password", nativeQuery = true)
 	public Account findAccount(@Param("username") String username, @Param("password") String password);
 
+	@Modifying
 	@Query(value = "insert into account(username,password) values (:p1,:p2)", nativeQuery = true)
 	public void createAccount(@Param("p1") String username, @Param("p2") String password);
 }
